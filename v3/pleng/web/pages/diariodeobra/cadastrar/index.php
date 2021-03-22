cc77f39f5842c8f7b6718a42f9ce124a65eae418
jnnastti <67485837+jnnastti@users.noreply.github.com>
2021-03-22 17:59:30 -0300

    Update index.php

--- v3/pleng/web/pages/diariodeobra/cadastrar/index.php
+++ v3/pleng/web/pages/diariodeobra/cadastrar/index.php
@@ -19,7 +19,7 @@
             <div class="titulo">
                 <h1> Cadastrar novo diário </h1>
             </div>
-                <form method="POST" name="cadastrodiariodeobra" action="" id="form1">
+                <form method="POST" name="cadastrodiariodeobra" action="../../../../server/src/diariodeobra/cadastrarDiario.php" id="form1">
                     <div class="cont parte1" id="parte1">
                         
                         <div class="cabecalho">
@@ -40,7 +40,7 @@
                         
                         <div class="field">
                             <label> Data: </label>
-                            <input type="date" name="data" />
+                            <input type="date" name="data_diario" />
                         </div>
 
                         <div class="fieldtempo">
@@ -601,7 +601,7 @@
                         </div>
                               
                         <div class="grid-12">
-                            <button type="button" class="btnCancelar"> Cancelar </button>
+                            <a href="index.php"><button type="button" class="btnCancelar"> Cancelar </button></a>
                             <div class="botoes">
                                 <button type="button" class="btnAvancar" onClick="onClickAvancar();"> Continuar </button>
                             </div>
@@ -635,101 +635,13 @@
                         <div class="grid-12">
                             <button type="button" class="btnCancelar" onClick="onClickVoltar();"> Voltar </button>
                             <div class="botoes">
-                                <button type="button" class="btnAvancar" onClick="onClickAvancar();"> Continuar </button>
+                                <button type="submit" class="btnAvancar" onClick="onClickAvancar();"> Continuar </button>
                             </div>
                         </div>
                               
                     </div> <!--- fecha parte2 --->
                 </form>
 
-                <form method="POST" name="cadastrodiariodeobra" action="" class="isHidden"  id="parte3">
-                    <div class="cont parte3">
-                        
-                        <div class="cabecalho">
-                        </div>
-
-                        <div class="infocab">
-                            <div class="grid-3">
-                                <img src="../../../assets/imgs/iconnum3.svg" />
-                            </div>
-                            <div class="grid-9">
-                                <h3> Etapas </h3>
-                                <p> 
-                                    Selecione a etapa e altere as informações relacionadas a sua realização, 
-                                    informando também a quantidade de material gastos para que seja calculado 
-                                    a quantidade de material restante.
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div class="field">
-                            <label> Etapa: </label>
-                            <select>
-                                <option>Nome...</option>
-                            </select>
-                        </div>
-
-                        <div class="items">
-                            <div class="item">
-                                <div class="field">
-                                    <label> Quantidade de material usado: </label>
-                                    <input type="text" name="qtdematusado" />
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="field">
-                                    <div class="field">
-                                        <label> Quantidade de material pedido: </label>
-                                        <input type="text" name="qtdematpedido" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="items">
-                            <div class="item">
-                                <div class="field">
-                                    <label> Quantidade de material usado até agora: </label>
-                                    <input type="text" name="qtdematatual" disabled />
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="field">
-                                    <div class="field">
-                                        <label> Quantidade de material a ser usado até a finalização: </label>
-                                        <input type="text" name="qtdemattotal" disabled />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="items">
-                            <div class="item">
-                                <div class="field">
-                                    <label> Tamanho concluido: </label>
-                                    <input type="text" name="tamanho" />
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="field">
-                                    <div class="field">
-                                        <label> Tamanho total: </label>
-                                        <input type="text" name="tamanhototal" disabled />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="grid-12">
-                            <button type="button" class="btnCancelar"> Cancelar </button>
-                            <div class="botoes">
-                                <button type="button" class="btnAvancar"> Finalizar </button>
-                                <button type="button" class="btnAvancar"> Salvar </button>
-                            </div>
-                        </div>
-                              
-                    </div> <!--- fecha parte3 --->
-                </form>
         </main>
     </div>
 
