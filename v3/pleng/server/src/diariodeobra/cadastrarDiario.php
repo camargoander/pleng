703b1153958b69685ad66c183e139c9fce729b69
jnnastti <67485837+jnnastti@users.noreply.github.com>
2021-03-22 17:53:23 -0300

    Add files via upload

---
+++ v3/pleng/server/src/diariodeobra/cadastrarDiario.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+class MyDB extends SQLite3
+{
+   function __construct()
+   {
+      $this->open('../../database/pleng.db');
+   }
+}
+
+$db = new MyDB();
+
+$data = $_POST['data_diario'];
+
+$segManha = $_POST['climasegundamanha'];
+$segTarde = $_POST['climasegundatarde'];
+$terManha = $_POST['climatercamanha'];
+$terTarde = $_POST['climatercatarde'];
+$quaManha = $_POST['climaquartamanha'];
+$quaTarde = $_POST['climaquartarde'];
+$quiManha = $_POST['climaquintamanha'];
+$quiTarde = $_POST['climaquintatarde'];
+$sexManha = $_POST['climasextamanha'];
+$sexTarde = $_POST['climasextatarde'];
+
+$levantamento = $_SESSION['idLevantamentoAtivo'];
+
+if(!$db) {
+    echo $db->lastErrorMsg();
+} else {
+
+    $result = $db->query("INSERT INTO diariodeobra(data_diario, segManha, segTarde, terManha, terTarde, quaManha, quaTarde, quiManha, quiTarde, sexManha, sexTarde, idlevantamento) 
+    VALUES ('".$data."', '".$segManha."', '".$segTarde."', '".$terManha."', '".$terTarde."', '".$quaManha."', '".$quaTarde."', '".$quiManha."', '".$quiTarde."', '".$sexManha."', '".$sexTarde."', $levantamento)");
+
+    $_SESSION['diarioIniciado'] = true;
+
+    header('location:../../../web/pages/diariodeobra/cadastrar/index.php');
+}
+
+
+?>
---
+++ v3/pleng/server/src/diariodeobra/listarDiario.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+class MyDB extends SQLite3
+{
+   function __construct()
+   {
+      $this->open('../../../server/database/pleng.db');
+   }
+}
+
+if(!$db) {
+    echo $db->lastErrorMsg();
+} else {
+
+    $result = $db->query('SELECT * FROM diariodeobra INNER JOIN levantamentoinicial ON levantamentoinicial.idproj = projeto.idproj');
+
+    while ($row = $result->fetchArray()) {
+        echo "
+        <tr>
+            <td> ".$row['nome']." </td>
+            <td class='data'> ".$row['data']." </td>
+            <td class='porcentagem'> ".$row['porcentagem']." </td>
+        </tr>
+        ";
+    }
+}
+
+?>
