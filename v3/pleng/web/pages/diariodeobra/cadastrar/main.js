8faf23a48a1d1db7638bde96bab9f06aa93b85f2
jnnastti <67485837+jnnastti@users.noreply.github.com>
2021-03-22 17:59:47 -0300

    Update main.js

--- v3/pleng/web/pages/diariodeobra/cadastrar/main.js
+++ v3/pleng/web/pages/diariodeobra/cadastrar/main.js
@@ -1,6 +1,5 @@
 var cont1 = document.getElementById('parte1');
 var cont2 = document.getElementById('parte2');
-var cont3 = document.getElementById('parte3');
 
 var form = document.getElementById('form1');
 
@@ -12,11 +11,6 @@ function onClickAvancar() {
         case 0: {
             contClick+=1;
             ativaCont2();
-            break;
-        }
-        case 1: {
-            contClick+=1;
-            ativaCont3();
             break;
         }
         default: {
@@ -33,11 +27,6 @@ function onClickVoltar() {
             ativaCont1();
             break;
         }
-        case 2: {
-            contClick-=1;
-            ativaCont2();
-            break;
-        }
         default: {
             break;
         }
@@ -48,20 +37,13 @@ function ativaCont1() {
     cont1.classList.remove('isHidden');
     form.classList.remove('isHidden');
     cont2.classList.add('isHidden');
-    cont3.classList.add('isHidden');
 }
 
 function ativaCont2() {
     cont2.classList.remove('isHidden');
     form.classList.remove('isHidden');
     cont1.classList.add('isHidden');
-    cont3.classList.add('isHidden');
 }
 
-function ativaCont3() {
-    cont3.classList.remove('isHidden');
-    cont2.classList.add('isHidden');
-    cont1.classList.add('isHidden');
-    form.classList.add('isHidden');
-}
+
 
