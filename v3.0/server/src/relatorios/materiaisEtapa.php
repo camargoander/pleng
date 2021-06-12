ff33407dc99f629eca8fba99fad7a5ca7369530e
jnnastti <67485837+jnnastti@users.noreply.github.com>
2021-06-12 16:30:48 -0300

    Update materiaisEtapa.php
    Utilizando fpdf pra gerar o relatório

--- v3.0/server/src/relatorios/materiaisEtapa.php
+++ v3.0/server/src/relatorios/materiaisEtapa.php
@@ -1,33 +1,94 @@
-<?php
+<?
 
-include("mpdf60/mpdf.php");
+session_start();
 
-$html = "
-<div class='grid-12'>
-<h1> Lista de materiais por etapa </h1>
+require('../../fpdf/fpdf.php');
 
-<br/>
+header("Content-type: text/html; charset=utf-8");
 
-<h3> nome da etapa </h3>
-<table>
-    <tr>
-        <td> Nome </td>
-        <td> Quantidade </td>
-    </tr>
-</table>
-</div>
-";
+class MyDB extends SQLite3
+{
+   function __construct()
+   {
+      $this->open('../../database/pleng.db');
+   }
+}
 
-$mpdf=new mPDF();
+class PDF extends FPDF
+{
 
-$mpdf->SetDisplayMode('fullpage');
+    // Simple table
+    function BasicTable($header)
+    {
+        $db = new MyDB();
+        $id = $_SESSION['idProjAtivo'];
 
-// add css
-$css = file_get_contents("css/estilo.css");
+        $result = $db->query("SELECT etapa_projeto.*, etapa.nome as nome_etapa
+                              FROM etapa_projeto, etapa WHERE etapa_projeto.idproj = $id
+                              AND etapa_projeto.idetapa = etapa.idetapa");
 
-$mpdf->WriteHTML($css,1);
-$mpdf->WriteHTML($html);
-$mpdf->Output();
+        while ($row = $result->fetchArray()) {
+            $this->SetFont('Arial','b',12);
+            $this->Cell(78,8,utf8_decode('> ' .$row['nome_etapa']),0, 0, 'L');
+            $this->Ln();
 
-exit;
-?>
+             // Header
+            foreach($header as $col) {
+                $this->SetFont('Arial','b',11);
+
+                if($col == 'Unidade') {
+                    $this->Cell(33,8,$col,1);
+                } else {
+                    $this->Cell(78,8,$col,1);
+                }
+            }
+
+            $this->Ln();
+
+            $resultMat = $db->query("SELECT etapa_projeto.idetapaproj, material.unidade, 
+                                etapa_projeto.tamanho_total, 
+                                material.nome, qtde_material.qtde, 
+                                (qtde_material.qtde * etapa_projeto.tamanho_total) as qtde_final 
+                                FROM etapa_projeto 
+                                INNER JOIN qtde_material ON etapa_projeto.idetapa = qtde_material.idetapa 
+                                INNER JOIN material ON qtde_material.idmat = material.idmat 
+                                WHERE qtde_material.idgrupo = etapa_projeto.idgrupo  
+                                AND etapa_projeto.idetapa = " .$row['idetapa']. "
+                                AND etapa_projeto.idetapaproj = " .$row['idetapaproj']);
+            
+            while($rowMaterial = $resultMat->fetchArray()) {
+                $this->SetFont('Arial','',10);
+
+                $this->Cell(78,8,utf8_decode($rowMaterial['nome']),1);
+                $this->Cell(33,8,utf8_decode($rowMaterial['unidade']),1);
+                $this->Cell(78,8,utf8_decode($rowMaterial['qtde_final']),1);
+
+                $this->Ln();
+            }   
+            $this->Ln();         
+        }   
+    }
+}
+
+$pdf = new PDF();
+
+// title
+$title = 'Relatório de materiais por etapa';
+$pdf->SetTitle(utf8_decode($title));
+
+// Iniciar pdf
+$pdf->AddPage();
+
+// titulo
+$pdf->SetFont('Arial','b',14);
+$pdf->Cell(190,10, utf8_decode($title), 0, 0, 'C');
+
+$pdf->Ln();
+$pdf->Ln();
+
+$header = array('Material', 'Unidade', utf8_decode('Quantidade total para a execução'));
+
+$pdf->BasicTable($header);
+
+$pdf->Output();
+?>  
