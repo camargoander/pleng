b6de128f7452ba2f3dd716d964e1a3b6b324519f
Jannaina Sangaletti <67485837+jnnastti@users.noreply.github.com>
2021-06-29 14:14:49 -0300

    Add files via upload

---
+++ v3.0/server/src/relatorios/diarioDeObraResumo.php
@@ -0,0 +1,76 @@
+<?
+
+session_start();
+
+require('../../fpdf/fpdf.php');
+
+header("Content-type: text/html; charset=utf-8");
+
+class MyDB extends SQLite3
+{
+   function __construct()
+   {
+      $this->open('../../database/pleng.db');
+   }
+}
+
+class PDF extends FPDF
+{
+
+    // Simple table
+    function BasicTable($header)
+    {
+        $db = new MyDB();
+        $id = $_SESSION['idProjAtivo'];
+
+         // Header
+         foreach($header as $col) {
+            $this->SetFont('Arial','b',11);
+
+            if($col == 'Data')  {
+                $this->Cell(30,8,$col,1);
+            } else if($col == 'Nome') {
+                $this->Cell(60,8,$col,1);
+            } else {
+                $this->Cell(100,8,$col,1);
+            }
+        }
+
+        $this->Ln();
+
+        $result = $db->query("SELECT * FROM diariodeobra WHERE idproj = $id");
+
+        while ($row = $result->fetchArray()) {
+                $this->SetFont('Arial','',10);
+
+                $this->Cell(30,8,utf8_decode($row['datadiario']),1);
+                $this->Cell(60,8,utf8_decode($row['nome']),1);
+                $this->Cell(100,8,utf8_decode($row['observacao']),1);
+
+                $this->Ln();     
+        }   
+    }
+}
+
+$pdf = new PDF();
+
+// title
+$title = 'Relatório de diário de obra';
+$pdf->SetTitle(utf8_decode($title));
+
+// Iniciar pdf
+$pdf->AddPage();
+
+// titulo
+$pdf->SetFont('Arial','b',14);
+$pdf->Cell(190,10, utf8_decode($title), 0, 0, 'C');
+
+$pdf->Ln();
+$pdf->Ln();
+
+$header = array('Data', 'Nome', utf8_decode('Observação'));
+
+$pdf->BasicTable($header);
+
+$pdf->Output();
+?>
