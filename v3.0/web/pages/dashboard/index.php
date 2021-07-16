35d27310b3350a457cbc64d9823e405c486b46b3
Jannaina Sangaletti <67485837+jnnastti@users.noreply.github.com>
2021-07-15 22:41:41 -0300

    Add files via upload

--- v3.0/web/pages/dashboard/index.php
+++ v3.0/web/pages/dashboard/index.php
@@ -16,72 +16,30 @@
 
             <main class="dash">
 
-                <div class="grid-12">
-                    <div class="titulo">
-                        <h1> Dashboard do projeto </h1>
+                <div class="grid-12 infos">
+                    <h1 class="titulo"> Dahsboards </h1>
+
+                    <h3> Percentual geral dos últimos projetos</h3>
+                    <img alt="Percentual" src="../../../server/src/dashboard/PercentualGeral.php" />
+
+                    <h3> Demais informações sobre o projeto atual </h3>
+                    <div class="grid-4 etapas">
+                        <h3> Etapa atual em andamento </h3>
+                        <?php include('../../../server/src/dashboard/etapasAndamento.php');?>
                     </div>
 
-                    <div class="grid-4">
-                        <h3> Quantidade gasta até o momento </h3>
-                        
-                        <?php
-                            SetFileFormat("png");
+                    <div class="grid-6">
+                        <h3> Gastos por etapas </h3>
 
-                            $grafico->SetTitle("Quantidade gasta até o momento");
-                            $grafico->SetXTitle("Mês");
-                            $grafico->SetYTitle("Valor");
-
-                            $dados = array(
-                                array('Janeiro', 10),
-                                array('Fevereiro', 5),
-                                array('Março', 4),
-                                array('Abril', 8),
-                                array('Maio', 7),
-                                array('Junho', 5),
-                            );
-
-                            // $graph->SetPlotType("pie");
-                            $grafico->SetDataValues($dados);
-                            $grafico->DrawGraph();
-                        ?>
-
+                        <img src="" />
                     </div>
 
-                    <div class="grid-8">
-                        <h3> Materiais usados e necessários até a conclusão </h3>
-
-                        <?php
-                            SetFileFormat("png");
-
-                            $dados = array(
-                                array('White', 10),
-                                array('Gold', 5),
-                                array('Silver', 4),
-                            );
-
-                            $plot = new PHPlot();
-
-                            $plot->SetPlotType('pie');
-                            $plot->SetDataType('text-data-single');
-                            $plot->SetDataValues($data);
-
-                            foreach ($data as $row) 
-                                $plot->SetLegend($row[0]); // Copy labels to legend
-                        
-                            $plot->DrawGraph();
-                        ?>
-
-
-                    </div>
+                    
+                
                 </div>
 
-                <div class="grid-12">
-                    <div class="grid-8">
-                        <h3> Etapas </h3>
-                    </div>
-                    <div class="grid-4">
-                        <h3> Porcentagem de conclusão do projeto </h3>
-                    </div>
+                <div class="grid-12 infos">
+                    <h3> Consumo de materiais </h3>
                 </div>
             </main>
         </div>
