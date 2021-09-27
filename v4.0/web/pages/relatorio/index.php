a00836805abe951649b0e0988e5d8b9cb9f32a98
jnnastti <janna.sangaletti6@gmail.com>
2021-09-27 10:42:42 -0300

    Conversão de HTML para PHP

---
+++ v4.0/web/pages/relatorio/index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+
+        <?php 
+            include('../../assets/cmp/principal/head.php');
+        ?>
+
+        <title> PLENG | Relatórios </title>
+    </head>
+    
+    <body>
+        <?php 
+            include('../../assets/cmp/principal/cabecalho.php');
+        ?>
+
+        <?php 
+            include('../../assets/cmp/principal/menulateral.php');
+        ?>
+
+        <main class="container">
+            <h1> Emitir relatórios do projeto </h1>
+
+            <section class="grid-12">
+                <div class="grid-3"> <i class="fa gg-file"></i> <span> Materiais por etapa </span> </div>
+                <div class="grid-3"> <i class="fa gg-file"></i> <span> Materiais do projeto </span> </div>
+                <div class="grid-3"> <i class="fa gg-file"></i> <span> Resumo de diários </span> </div>
+                <div class="grid-3"> <i class="fa gg-file"></i> <span> Manual de usuário </span> </div>
+            </section>
+        </main>
+    </body>
+</html>
