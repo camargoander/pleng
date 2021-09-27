451050a8b3313b39d597131a0b5482a4f68ee9fc
jnnastti <janna.sangaletti6@gmail.com>
2021-09-27 10:39:38 -0300

    Iniciando conversão de HTML para PHP

---
+++ v4.0/web/pages/menu/index.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+
+        <?php 
+            include('../../assets/cmp/principal/head.php');
+        ?>
+
+        <title> PLENG | Planejando um futuro melhor ao seu lado </title>
+    </head>
+
+    <body>
+
+        <?php 
+            include('../../assets/cmp/principal/cabecalho.php');
+        ?>
+
+        <?php 
+            include('../../assets/cmp/principal/menulateral.php');
+        ?>
+
+        <main class="container">
+            <h1> Projeto: nome do projeto </h1>
+
+            <section class="grid-12">
+                <div class="grid-3"> <i class="fa gg-toolbox"></i> <span> Configurar </span> </div>
+                <div class="grid-3"> <i class="fa gg-trello"></i> <span> Dashboards </span> </div>
+                <div class="grid-3"> <i class="fa gg-file"></i> <span> Relatórios </span> </div>
+                <div class="grid-3"> <i class="fa gg-calculator"></i> <span> Orçamento </span> </div>
+                <div class="grid-3"> <i class="fa gg-album"></i> <span> Levantamento inicial </span> </div>
+                <div class="grid-3"> <i class="fa gg-list"></i> <span> Diário de obras </span> </div>
+                <div class="grid-3"> <i class="fa gg-calendar-today"></i> <span> Cronograma </span> </div>
+                <div class="grid-3"> <i class="fa gg-image"></i> <span> Galeria </span> </div>
+                <div class="grid-3"> <i class="fa gg-terminal"></i> <span> Mais opções</span> </div>
+            </section>
+        </main>
+    </body>
+</html>
