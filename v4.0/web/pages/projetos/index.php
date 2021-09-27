3eb96a338086f5dc3dd923721711d51e0b5fbf2c
jnnastti <janna.sangaletti6@gmail.com>
2021-09-27 10:49:27 -0300

    Convertendo de HTML para PHP

---
+++ v4.0/web/pages/projetos/index.php
@@ -0,0 +1,61 @@
+<html>
+    <head>
+
+        <?php 
+            include('../../assets/cmp/principal/head.php');
+        ?>
+
+        <title> PLENG | Seus projetos </title>
+    </head>
+    <body>
+
+        <?php 
+            include('../../assets/cmp/principal/cabecalho.php');
+        ?>
+
+        <?php 
+            include('../../assets/cmp/principal/menulateral.php');
+        ?>
+
+        <main class="container">
+            <h1> Selecione seu projeto ou inicie um novo </h1>
+
+            <section class="pesquisa grid-12">
+                <div class="grid-3">
+                    <button> Cadastrar projeto </button>
+                </div>
+                <div class="grid-6">
+                    <input type="text" placeholder="Nome do seu projeto" />
+                    <button> <i class="gg-search"></i> </button>
+                </div>
+            </section>
+
+            <section class="projetos grid-12">
+                <div class="grid-3">
+                    <h3> Nome do projeto </h3>
+                    <p><b> Local: </b>Rua Pedro Doneda, 73 </p>
+                    <p><b>Responsável: </b> Batatinha </p>
+                    <button> Selecionar </button>
+                </div>
+                <div class="grid-3">
+                    <h3> Nome do projeto </h3>
+                    <p><b> Local: </b>Rua Pedro Doneda, 73 </p>
+                    <p><b>Responsável: </b> Batatinha </p>
+                    <button> Selecionar </button>
+                </div>
+                <div class="grid-3">
+                    <h3> Nome do projeto </h3>
+                    <p><b> Local: </b>Rua Pedro Doneda, 73 </p>
+                    <p><b>Responsável: </b> Batatinha </p>
+                    <button> Selecionar </button>
+                </div>
+                <div class="grid-3">
+                    <h3> Nome do projeto </h3>
+                    <p><b> Local: </b>Rua Pedro Doneda, 73 </p>
+                    <p><b>Responsável: </b> Batatinha </p>
+                    <button> Selecionar </button>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
