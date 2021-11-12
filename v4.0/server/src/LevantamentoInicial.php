10a95dd7a91c0db8ca4533e79a855b17403df40c
jnnastti <janna.sangaletti6@gmail.com>
2021-11-12 19:15:19 -0300

    CRUD levantamento inicial

--- v4.0/server/src/LevantamentoInicial.php
+++ v4.0/server/src/LevantamentoInicial.php
@@ -11,15 +11,15 @@ class LevantamentoInicial
 
     public function cadastrarLevantamentoInicial(object $levantamento)
     {
-        $insertLevantamento = $this->sqlite->prepare('INSERT INTO levantamento_inicial(idetapa, tamanhototal, posicao, situacao, descricao, idproj)
-                                                VALUES(:idetapa, :tamanhototal, :posicao, :situacao, :descricao, :idproj)');
+        $insertLevantamento = $this->sqlite->prepare('INSERT INTO levantamento_inicial(idetapa, idprojeto, situacao, descricao, tamanho_total, tamanho_atual)
+                                                VALUES(:idetapa, :idproj, :situacao, :descricao, :tamanhototal, :tamanhoatual)');
         
         $insertLevantamento->bindParam(':idetapa', $levantamento->idetapa);
-        $insertLevantamento->bindParam(':tamanhototal', $levantamento->tamanhototal);
-        $insertLevantamento->bindParam(':posicao', $levantamento->posicao);
+        $insertLevantamento->bindParam(':idproj', $levantamento->idproj);
         $insertLevantamento->bindParam(':situacao', $levantamento->situacao);
         $insertLevantamento->bindParam(':descricao', $levantamento->descricao);
-        $insertLevantamento->bindParam(':idproj', $levantamento->idproj);
+        $insertLevantamento->bindParam(':tamanhototal', $levantamento->tamanhototal);
+        $insertLevantamento->bindParam(':tamanhoatual', $levantamento->tamanhoatual);
 
         $insertLevantamento->execute();
     }
@@ -27,14 +27,12 @@ class LevantamentoInicial
     public function editarLevantamentoInicial(object $levantamento)
     {
         $updateLevantamento = $this->sqlite->prepare('UPDATE levantamento_inicial SET
-                                                        tamanhototal = :tamanhototal,
-                                                        posicao = :posicao,
+                                                        tamanho_total = :tamanhototal,
                                                         situacao = :situacao,
                                                         descricao = :descricao
                                                     WHERE idlevantamento = :idlevantamento');
         
         $updateLevantamento->bindParam(':tamanhototal', $levantamento->tamanhototal);
-        $updateLevantamento->bindParam(':posicao', $levantamento->posicao);
         $updateLevantamento->bindParam(':situacao', $levantamento->situacao);
         $updateLevantamento->bindParam(':descricao', $levantamento->descricao);
         $updateLevantamento->bindParam(':idlevantamento', $levantamento->idlevantamento);
@@ -53,7 +51,12 @@ class LevantamentoInicial
 
     public function listarLevantamentoInicialAndamento(int $idprojeto)
     {
-        $selectLevantamentoAndamento = $this->sqlite->prepare('SELECT * FROM levantamento_inicial WHERE idprojeto = :idprojeto AND situacao = "A"');
+        $selectLevantamentoAndamento = $this->sqlite->prepare('SELECT levantamento_inicial.*, etapa.nome  
+                                                                    FROM levantamento_inicial 
+                                                                    INNER JOIN etapa 
+                                                                    ON levantamento_inicial.idetapa = etapa.idetapa 
+                                                                    WHERE idprojeto = :idprojeto 
+                                                                    AND situacao = "A"');
 
         $selectLevantamentoAndamento->bindParam(':idprojeto', $idprojeto);
 
@@ -64,7 +67,12 @@ class LevantamentoInicial
 
     public function listarLevantamentoInicialFinalizado(int $idprojeto)
     {
-        $selectLevantamentoFinalizado = $this->sqlite->prepare('SELECT * FROM levantamento_inicial WHERE idprojeto = :idprojeto AND situacao = "F"');
+        $selectLevantamentoFinalizado = $this->sqlite->prepare('SELECT levantamento_inicial.*, etapa.nome  
+                                                                    FROM levantamento_inicial 
+                                                                    INNER JOIN etapa 
+                                                                    ON levantamento_inicial.idetapa = etapa.idetapa 
+                                                                    WHERE idprojeto = :idprojeto 
+                                                                    AND situacao = "F"');
 
         $selectLevantamentoFinalizado->bindParam(':idprojeto', $idprojeto);
 
@@ -73,15 +81,86 @@ class LevantamentoInicial
         return $levantamentoFinalizado;
     }
 
-    public function listarLevantamentoInicialNaoIniciada(int $idprojeto)
+    public function listarLevantamentoInicialNaoIniciado(int $idprojeto)
     {
-        $selectLevantamentoNaoIniciada = $this->sqlite->prepare('SELECT * FROM levantamento_inicial WHERE idprojeto = :idprojeto AND situacao = "N"');
+        $selectLevantamentoNaoIniciada = $this->sqlite->prepare('SELECT levantamento_inicial.*, etapa.nome  
+                                                                    FROM levantamento_inicial 
+                                                                    INNER JOIN etapa 
+                                                                    ON levantamento_inicial.idetapa = etapa.idetapa 
+                                                                    WHERE idprojeto = :idprojeto 
+                                                                    AND situacao = "N"');
 
         $selectLevantamentoNaoIniciada->bindParam(':idprojeto', $idprojeto);
 
         $levantamentoNaoIniciada = $selectLevantamentoNaoIniciada->execute();
 
         return $levantamentoNaoIniciada;
+    }
+
+    public function listarLevantamentoInicialAndamentoFiltrado(int $idprojeto, int $idetapa)
+    {
+        $selectLevantamentoAndamento = $this->sqlite->prepare('SELECT levantamento_inicial.*, etapa.nome  
+                                                                    FROM levantamento_inicial 
+                                                                    INNER JOIN etapa 
+                                                                    ON levantamento_inicial.idetapa = etapa.idetapa 
+                                                                    WHERE idprojeto = :idprojeto 
+                                                                    AND situacao = "A"
+                                                                    AND levantamento_inicial.idetapa = :idetapa');
+
+        $selectLevantamentoAndamento->bindParam(':idprojeto', $idprojeto);
+        $selectLevantamentoAndamento->bindParam(':idetapa', $idetapa);
+
+        $levantamentoAndamento = $selectLevantamentoAndamento->execute();
+
+        return $levantamentoAndamento;
+    }
+
+    public function listarLevantamentoInicialFinalizadoFiltrado(int $idprojeto, int $idetapa)
+    {
+        $selectLevantamentoFinalizado = $this->sqlite->prepare('SELECT levantamento_inicial.*, etapa.nome  
+                                                                    FROM levantamento_inicial 
+                                                                    INNER JOIN etapa 
+                                                                    ON levantamento_inicial.idetapa = etapa.idetapa 
+                                                                    WHERE idprojeto = :idprojeto 
+                                                                    AND situacao = "F"
+                                                                    AND levantamento_inicial.idetapa = :idetapa');
+
+        $selectLevantamentoFinalizado->bindParam(':idprojeto', $idprojeto);
+        $selectLevantamentoFinalizado->bindParam(':idetapa', $idetapa);
+
+        $levantamentoFinalizado = $selectLevantamentoFinalizado->execute();
+
+        return $levantamentoFinalizado;
+    }
+
+    public function listarLevantamentoInicialNaoIniciadoFiltrado(int $idprojeto, int $idetapa)
+    {
+        $selectLevantamentoNaoIniciada = $this->sqlite->prepare('SELECT levantamento_inicial.*, etapa.nome  
+                                                                    FROM levantamento_inicial 
+                                                                    INNER JOIN etapa 
+                                                                    ON levantamento_inicial.idetapa = etapa.idetapa 
+                                                                    WHERE idprojeto = :idprojeto 
+                                                                    AND situacao = "N"
+                                                                    AND levantamento_inicial.idetapa = :idetapa');
+
+        $selectLevantamentoNaoIniciada->bindParam(':idprojeto', $idprojeto);
+        $selectLevantamentoNaoIniciada->bindParam(':idetapa', $idetapa);
+
+        $levantamentoNaoIniciada = $selectLevantamentoNaoIniciada->execute();
+
+        return $levantamentoNaoIniciada;
+    }
+
+    public function selecionarLevantamentoInicial(int $id)
+    {
+        $selectLevantamentoEspecifico = $this->sqlite->prepare('SELECT * FROM levantamento_inicial
+                                                                    WHERE idlevantamento = :id');
+
+        $selectLevantamentoEspecifico->bindParam(':id', $id);
+
+        $levantamentoEspecifico = $selectLevantamentoEspecifico->execute()->fetchArray();
+
+        return $levantamentoEspecifico;
     }
 }
 
--- v4.0/web/pages/levantamentoinicial/index.php
+++ v4.0/web/pages/levantamentoinicial/index.php
@@ -1,5 +1,79 @@
+<?php
+
+    require('../../../server/config/conexao.php');
+    require('../../../server/config/redireciona.php');
+
+    include('../../../server/src/LevantamentoInicial.php');
+    include('../../../server/src/Etapa.php');
+
+    if(!isset($_SESSION['usuario'])) {
+        redireciona('../login/login.php');
+
+        return;
+    }
+    if(!isset($_SESSION['projeto'])) {
+        redireciona('../projetos/index.php');
+
+        return;
+    }
+
+    $levantamentos = new LevantamentoInicial($db);
+    
+    $levantamentoAndamento = $levantamentos->listarLevantamentoInicialAndamento($_SESSION['projeto']);
+    $levantamentoFinalizado = $levantamentos->listarLevantamentoInicialFinalizado($_SESSION['projeto']);
+    $levantamentoNaoIniciado = $levantamentos->listarLevantamentoInicialNaoIniciado($_SESSION['projeto']);
+
+    $action = (isset($_REQUEST['action'] )) ? $_REQUEST['action']  : '';
+
+    $info = (isset($_GET['id'])) ? $levantamentos->selecionarLevantamentoInicial($_GET['id']) : '';
+
+    switch($action) {
+        
+        case 'deletar': {
+            $levantamentos->deletarLevantamentoInicial($_GET['id']);
+
+            redireciona('./index.php');
+            break;
+        }
+
+
+        case 'editar': {
+            $etapaDados = (object) array (
+                'idlevantamento' => $_POST['id'],
+                'situacao' => $_POST['situacao'],
+                'descricao' => $_POST['descricao'],
+                'tamanhototal' => $_POST['tamanhoTotal']
+            );
+    
+            $levantamentos->editarLevantamentoInicial($etapaDados);
+    
+            redireciona('./index.php');
+
+            break;
+        }
+
+        case 'filtrar': {
+            if($_POST['etapa'] !== "") {
+                $levantamentoAndamento = $levantamentos->listarLevantamentoInicialAndamentoFiltrado($_SESSION['projeto'], $_POST['etapa']);
+                $levantamentoFinalizado = $levantamentos->listarLevantamentoInicialFinalizadoFiltrado($_SESSION['projeto'], $_POST['etapa']);
+                $levantamentoNaoIniciado = $levantamentos->listarLevantamentoInicialNaoIniciadoFiltrado($_SESSION['projeto'], $_POST['etapa']);
+                $action = '';
+            }
+
+            break;
+        }
+    }
+
+    $etapas = new Etapa($db);
+
+    $etapa = $etapas->listarEtapa();
+?>
+
 <html>
     <head>
+
+        <link href="../../assets/styles/stylePopup.css" rel="stylesheet" />
+        <link href="../../assets/styles/formulario.css" rel="stylesheet" />
 
         <?php 
             include('../../assets/cmp/principal/head.php');
@@ -35,39 +109,108 @@
 
             <section class="pesquisa grid-12">
                 <div class="grid-3">
-                    <button> Nova etapa </button>
+                    <a href="../configurar/levantamentoinicial/index.php"><button> Nova etapa </button></a>
                 </div>
                 <div class="grid-6">
-                    <select>
-                        <option> Etapa </option>
+                    <form method="POST" action="./index.php?action=filtrar">
+                    <select name="etapa">
+                        <option value=""> Etapa </option>
+                        <?php while($etp = $etapa->fetchArray()) : ?>
+                            <option value="<?= $etp['idetapa']?>"> <?= $etp['nome']; ?></option>
+                        <?php endwhile; ?>
                     </select>
-                    <button> <i class="gg-search"></i> </button>
+                    <button type="submit"> <i class="gg-search"></i> </button>
+                    </form>
                 </div>
             </section>
 
-            <section class="grid-12">
+            <section class="grid-12 cont">
                 <h3> Etapas em andamento </h3>
                 <div class="etapas">
-                    <div class="grid-3"> <i class="fa gg-user-list"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-user-list"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-user-list"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-user-list"></i> <span> Nome etapa </span> </div>
+
+                    <?php while($liAndamento = $levantamentoAndamento->fetchArray()) : ?>
+                        <a class="grid-3" href="?id=<?= $liAndamento['idlevantamento']; ?>#editarModal">
+                            <div> 
+                                <i class="fa gg-user-list"></i> 
+                                <span> <?= $liAndamento['nome']; ?> </span> 
+                            </div>
+                        </a>
+                    <?php endwhile; ?>
                 </div>
                 <h3> Etapas não iniciadas </h3>
                 <div class="etapas">
-                    <div class="grid-3"> <i class="fa gg-sand-clock"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-sand-clock"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-sand-clock"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-sand-clock"></i> <span> Nome etapa </span> </div>
+                    <?php while($liNaoIniciado = $levantamentoNaoIniciado->fetchArray()) : ?>
+                        <a class="grid-3" href="?id=<?= $liNaoIniciado['idlevantamento']; ?>#editarModal">
+                            <div> 
+                                <i class="fa gg-sand-clock"></i> 
+                                <span> <?= $liNaoIniciado['nome']; ?> </span> 
+                            </div>
+                        </a>
+                    <?php endwhile; ?>
                 </div>
                 <h3> Etapas finalizadas </h3>
                 <div class="etapas">
-                    <div class="grid-3"> <i class="fa gg-check-r"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-check-r"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-check-r"></i> <span> Nome etapa </span> </div>
-                    <div class="grid-3"> <i class="fa gg-check-r"></i> <span> Nome etapa </span> </div>
+                    <?php while($liFinalizado = $levantamentoFinalizado->fetchArray()) : ?>
+                        <a class="grid-3" href="?id=<?= $liFinalizado['idlevantamento']; ?>#editarModal">
+                            <div> 
+                                <i class="fa gg-check-r"></i> 
+                                <span> <?= $liFinalizado['nome']; ?> </span> 
+                            </div>
+                        </a>
+                    <?php endwhile; ?>
                 </div>
             </section>
+
+            <!-- popup de editar -->
+            <div id="editarModal" class="modalDialog">
+                <div>
+                    <a href="#" title="Close" class="close">
+                        <div class="close-container">
+                            <div class="leftright"></div>
+                            <div class="rightleft"></div>
+                        </div>
+                    </a>
+                    <h2>Etapa</h2>
+
+                    <form method="POST" action="./index.php?action=editar">
+                        <input type="hidden" name="id" value="<?= $_GET['id']; ?>" />
+
+                        <fieldset>
+                            <label> Descrição </label>
+                            <textarea name="descricao"> <?= $info['descricao']; ?> </textarea>
+                        </fieldset>
+                        
+                        <div class="items">
+                            <div class="item">
+                                <fieldset>
+                                    <label> Tamanho atual </label>
+                                    <select name="situacao">
+                                        <option <?= ($info['situacao']) == 'A' ? 'selected' : ""; ?> value="A"> Andamento </option>
+                                        <option <?= ($info['situacao']) == 'N' ? 'selected' : ""; ?> value="N"> Não iniciada </option>
+                                        <option <?= ($info['situacao']) == 'F' ? 'selected' : ""; ?> value="F"> Finalizada </option>
+                                    </select>
+                                </fieldset>
+                            </div>
+                            <div class="item">
+                                <fieldset>
+                                    <label> Tamanho total </label>
+                                    <input type="text" name="tamanhoTotal" value="<?= $info['tamanho_total']; ?>" />
+                                <fieldset>    
+                            </div>
+                        </div>
+
+                        <div class="items">
+                            <div class="item">
+                                <a href="?action=deletar&id=<?= $_GET['id']; ?>"><button type="button" class="btnExcluir"> Excluir </button></a>
+                            </div>
+                            <div class="item">
+                                <button type="submit" class="btnPrincipal"> Cadastrar </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         </main>
     </body>
 </html>
